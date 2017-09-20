b724f2455a52ea26d5f0bfccea8f605f52dadcca
maud gennart <maud.gennart@imio.be>
2017-09-20 17:26:35 +0200

    add css for navigation

---
+++ cpskin/diazotheme/spirit/static/js/cpskin_spirit.js
@@ -0,0 +1,20 @@
+/*
+ * cpskin_spirit.js
+ * Copyright (C) 2017 AuroreMariscal <aurore@affinitic.be>
+ *
+ * Distributed under terms of the LICENCE.txt license.
+ */
+$(document).ready(function() {
+    var $window = $(window);
+
+// search
+    $( ".btn-search" ).click(function(event) {
+      $("#hidden-search").toggleClass("portal-search-visible portal-search-hidden");
+      event.preventDefault();
+    });
+       
+
+});
+
+
+
