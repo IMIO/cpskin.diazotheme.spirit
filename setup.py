42b6a29b8cedf32222f2f28f3b24af2926b835d9
maud gennart <maud.gennart@imio.be>
2017-12-08 15:29:21 +0100

    Back to development: 0.1.15

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Installer for the cpskin.diazotheme.spirit package."""
 
-version = '0.1.14'
+version = '0.1.15.dev0'
 
 from setuptools import find_packages
 from setuptools import setup
