090c7a66d4b2af1970b7532ae8097e4e6df6bc5c
Laurent Lasudry <lasudry@gmail.com>
2022-01-20 14:44:34 +0100

    Back to development: 0.1.48

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Installer for the cpskin.diazotheme.spirit package."""
 
-version = '0.1.47'
+version = '0.1.48.dev0'
 
 from setuptools import find_packages
 from setuptools import setup
